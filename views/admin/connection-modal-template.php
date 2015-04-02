cfaae95fc8d2b37292b7e3e64f499ee3468469b1
jessefriedman <jesserfriedman@gmail.com>
2015-04-02 11:12:42 -0400

    fixed logic of modal if statement

--- views/admin/connection-modal-template.php
+++ views/admin/connection-modal-template.php
@@ -6,40 +6,45 @@
 			<h2>Your Jetpack Connection</h2>
 			<table>
 				<tr>
-					<th>User</th>
-					<th>WordPress.com Username</th>
-					<th>Actions</th>
+					<th><?php _e( 'User', 'jetpack' ); ?></th>
+					<th><?php _e( 'WordPress.com Username', 'jetpack' ); ?></th>
+					<th><?php _e( 'Actions', 'jetpack' ); ?></th>
 				</tr>
-				<# if ( data.isMasterUser && data.isAdmin ) { #>
-				<tr>
-					<td><strong><?php esc_html_e('You (primary)', 'jetpack'); ?></strong></td>
-					<td><strong>{{{ data.masterComData.login }}}</strong></td>
-					<td><a class="button" href="<?php echo wp_nonce_url( Jetpack::admin_url( 'action=disconnect' ), 'jetpack-disconnect' ); ?>" onclick="return confirm('<?php echo htmlspecialchars( __('Are you sure you want to disconnect from WordPress.com?', 'jetpack'), ENT_QUOTES ); ?>');"><?php esc_html_e( 'Disconnect', 'jetpack' ); ?></a></td>
-				</tr>
+				<# if ( data.isAdmin ) { #>
+					<# if ( data.isMasterUser ) { #>
+					<tr>
+						<td><strong><?php esc_html_e( 'You (primary)', 'jetpack' ); ?></strong></td>
+						<td><strong>{{{ data.masterComData.login }}}</strong></td>
+						<td><a class="button" href="<?php echo wp_nonce_url( Jetpack::admin_url( 'action=disconnect' ), 'jetpack-disconnect' ); ?>" onclick="return confirm('<?php echo htmlspecialchars( __('Are you sure you want to disconnect from WordPress.com?', 'jetpack'), ENT_QUOTES ); ?>');"><?php esc_html_e( 'Disconnect', 'jetpack' ); ?></a></td>
+					</tr>
+					<# } else { #>
+					<tr>
+						<td><strong>{{{ data.masterUserLink }}}<?php esc_html_e( ' (primary)', 'jetpack' ); ?></strong></td>
+						<td><strong>{{{ data.masterComData.login }}}</strong></td>
+						<td>&nbsp;</td>
+					</tr>
+					<tr>
+						<td><?php esc_html_e( 'You', 'jetpack' ); ?></td>
+						<# if ( data.isUserConnected ) { #>
+							<td>{{{ data.userComData.login }}}</td>
+							<td><a id="set-self-as-master" class="button primary"><?php esc_html_e( 'Make primary account', 'jetpack' ); ?></a><span class="spinner"></span><a class="button" href="<?php echo wp_nonce_url( Jetpack::admin_url( 'action=unlink' ), 'jetpack-unlink' ); ?>"><?php esc_html_e( ' Disconnect', 'jetpack' ); ?></a></td>
+						<# } else { #>
+							<td>&nbsp;</td>
+							<td><a href="<?php echo Jetpack::init()->build_connect_url() ?>" class="button"><?php esc_html_e( 'Link your account', 'jetpack' ); ?></a></td>
+						<# } #>
+					</tr>
+					<# } #>
 				<# } else { #>
 				<tr>
-					<td><strong>{{{ data.masterUserLink }}}<?php esc_html_e(' (primary)', 'jetpack'); ?></strong></td>
-					<td><strong>{{{ data.masterComData.login }}}</strong></td>
-					<td>&nbsp;</td>
-				</tr>
-					<# if ( data.isActive ) { #>
-						<tr>
-							<td><?php esc_html_e('You', 'jetpack'); ?></td>
-							<# if ( data.isUserConnected ) { #>
-								<td><strong>{{{ data.userComData.login }}}</strong></td>
-								<td>
-									<# if ( data.isAdmin ) { #>
-										<a id="set-self-as-master" class="button primary"><?php esc_html_e( 'Make primary account', 'jetpack' ); ?></a><span class="spinner"></span>
-									<# } #>
-										<a class="button" href="<?php echo wp_nonce_url( Jetpack::admin_url( 'action=unlink' ), 'jetpack-unlink' ); ?>"><?php esc_html_e( ' Disconnect', 'jetpack' ); ?></a>
-								</td>
-							<# } else { #>
-								<td>&nbsp;</td>
-								<td><a href="<?php echo Jetpack::init()->build_connect_url() ?>" class="button"><?php esc_html_e( 'Link your account', 'jetpack' ); ?></a></td>
-							<# } #>
-
-						</tr>
+					<td><?php _e( 'You', 'jetpack' ); ?></td>
+					<# if ( data.isUserConnected ) { #>
+						<td><strong>{{{ data.userComData.login }}}</strong></td>
+						<td><a class="button" href="<?php echo wp_nonce_url( Jetpack::admin_url( 'action=unlink' ), 'jetpack-unlink' ); ?>"><?php esc_html_e( ' Disconnect', 'jetpack' ); ?></a></td>
+					<# } else { #>
+						<td>&nbsp;</td>
+						<td><a href="<?php echo Jetpack::init()->build_connect_url() ?>" class="button"><?php esc_html_e( 'Link your account', 'jetpack' ); ?></a></td>
 					<# } #>
+				</tr>
 				<# } #>
 			</table>
 		</div>

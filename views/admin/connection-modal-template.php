b268c050ec0952578cf2645c741054bc909888e6
jessefriedman <jesserfriedman@gmail.com>
2015-04-02 11:39:26 -0400

    added inline comments to modal if logic

--- views/admin/connection-modal-template.php
+++ views/admin/connection-modal-template.php
@@ -10,14 +10,14 @@
 					<th><?php _e( 'WordPress.com Username', 'jetpack' ); ?></th>
 					<th><?php _e( 'Actions', 'jetpack' ); ?></th>
 				</tr>
-				<# if ( data.isAdmin ) { #>
-					<# if ( data.isMasterUser ) { #>
+				<# if ( data.isAdmin ) { #><?php /* if user has admin privledges */ ?>
+					<# if ( data.isMasterUser ) { #><?php /* if user is the owner of the primary jetpack connection */ ?>
 					<tr>
 						<td><strong><?php esc_html_e( 'You (primary)', 'jetpack' ); ?></strong></td>
 						<td><strong>{{{ data.masterComData.login }}}</strong></td>
-						<td><a class="button" href="<?php echo wp_nonce_url( Jetpack::admin_url( 'action=disconnect' ), 'jetpack-disconnect' ); ?>" onclick="return confirm('<?php echo htmlspecialchars( __('Are you sure you want to disconnect from WordPress.com?', 'jetpack'), ENT_QUOTES ); ?>');"><?php esc_html_e( 'Disconnect', 'jetpack' ); ?></a></td>
+						<td><a class="button" href="<?php echo wp_nonce_url( Jetpack::admin_url( 'action=disconnect' ), 'jetpack-disconnect' ); ?>" onclick="return confirm('<?php echo htmlspecialchars( __( 'Are you sure you want to disconnect from WordPress.com?', 'jetpack' ), ENT_QUOTES ); ?>');"><?php esc_html_e( 'Disconnect', 'jetpack' ); ?></a></td>
 					</tr>
-					<# } else { #>
+					<# } else { #><?php /* user is an admin but not the owner primary jetpack connection */ ?>
 					<tr>
 						<td><strong>{{{ data.masterUserLink }}}<?php esc_html_e( ' (primary)', 'jetpack' ); ?></strong></td>
 						<td><strong>{{{ data.masterComData.login }}}</strong></td>
@@ -25,27 +25,27 @@
 					</tr>
 					<tr>
 						<td><?php esc_html_e( 'You', 'jetpack' ); ?></td>
-						<# if ( data.isUserConnected ) { #>
+						<# if ( data.isUserConnected ) { #><?php /* user is connected to Jetpack */ ?>
 							<td>{{{ data.userComData.login }}}</td>
 							<td><a id="set-self-as-master" class="button primary"><?php esc_html_e( 'Make primary account', 'jetpack' ); ?></a><span class="spinner"></span><a class="button" href="<?php echo wp_nonce_url( Jetpack::admin_url( 'action=unlink' ), 'jetpack-unlink' ); ?>"><?php esc_html_e( ' Disconnect', 'jetpack' ); ?></a></td>
-						<# } else { #>
+						<# } else { #><?php /* user isn't connected to Jetpack at all and should see a connection prompt */ ?>
 							<td>&nbsp;</td>
 							<td><a href="<?php echo Jetpack::init()->build_connect_url() ?>" class="button"><?php esc_html_e( 'Link your account', 'jetpack' ); ?></a></td>
-						<# } #>
+						<# } #><?php /* end data.isUserConnected */ ?>
 					</tr>
-					<# } #>
-				<# } else { #>
+					<# } #><?php /* end data.isMasterUser */ ?>
+				<# } else { #><?php /* User doesn't have admin privledges */ ?>
 				<tr>
 					<td><?php _e( 'You', 'jetpack' ); ?></td>
-					<# if ( data.isUserConnected ) { #>
+					<# if ( data.isUserConnected ) { #><?php /* user is connected to Jetpack */ ?>
 						<td><strong>{{{ data.userComData.login }}}</strong></td>
 						<td><a class="button" href="<?php echo wp_nonce_url( Jetpack::admin_url( 'action=unlink' ), 'jetpack-unlink' ); ?>"><?php esc_html_e( ' Disconnect', 'jetpack' ); ?></a></td>
-					<# } else { #>
+					<# } else { #><?php /* user isn't connected to Jetpack at all and should see a connection prompt */ ?>
 						<td>&nbsp;</td>
 						<td><a href="<?php echo Jetpack::init()->build_connect_url() ?>" class="button"><?php esc_html_e( 'Link your account', 'jetpack' ); ?></a></td>
-					<# } #>
+					<# } #><?php /* end data.isUserConnected */ ?>
 				</tr>
-				<# } #>
+				<# } #><?php /* end data.isAdmin */ ?>
 			</table>
 		</div>
 	</div>

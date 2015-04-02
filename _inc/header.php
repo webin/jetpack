64ac293465ba8c2c5c19f254a7a5b62b3eab0fe4
jessefriedman <jesserfriedman@gmail.com>
2015-04-02 10:07:34 -0400

    Modal Design updates
    Removed unnecessary styles from the header
    added a href=“#” to the “My connection” button
    changed the data views in the modal to be tabular for a better
    experience

--- _inc/header.php
+++ _inc/header.php
@@ -3,32 +3,6 @@
 	<div class="jp-frame">
 		<div class="header">
 			<nav role="navigation" class="header-nav drawer-nav nav-horizontal">
-				<style>/*
-					.jp-button--settings {
-						cursor: pointer;
-					}
-
-					.jp-modal-wrapper {
-						display: none;
-						position: absolute;
-						left: 0px;
-						top: 0px;
-						width:100%;
-						height:100%;
-						text-align:center;
-						z-index: 1000;
-					}
-
-					.jp-modal-wrapper div {
-						width:300px;
-						margin: 100px auto;
-						background-color: #fff;
-						border:1px solid #000;
-						padding:15px;
-						text-align:center;
-					}
-				*/
-				</style>
 				<div class="main-nav">
 					<div class="jetpack-logo">
 						<a href="<?php echo Jetpack::admin_url(); ?>" title="<?php esc_attr_e( 'Jetpack', 'jetpack' ); ?>" <?php if ( 'jetpack' == $current ) { echo 'class="current"'; } ?>><span><?php esc_html_e( 'Jetpack', 'jetpack' ); ?></span></a>
@@ -39,7 +13,7 @@
 					 <div class="options">
 						<a title="View your Jetpack settings" href="<?php echo Jetpack::admin_url( 'page=jetpack_modules' ); ?>" class="jp-button--settings <?php if ( 'jetpack_modules' == $current ) { echo 'current'; } ?>"><?php esc_html_e( 'Settings', 'jetpack' ); ?></a>
 						<a title="Give Jetpack feedback" href="http://jetpack.me/survey/?rel=<?php echo JETPACK__VERSION; ?>" class="jp-button--settings"><?php esc_html_e( 'Feedback', 'jetpack' ); ?></a>
-						<a title="View your Jetpack connection details" id="jp-connection" class="jp-button--settings"><?php esc_html_e( 'My Connection', 'jetpack' ); ?></a>
+						<a title="View your Jetpack connection details" href="#" id="jp-connection" class="jp-button--settings"><?php esc_html_e( 'My Connection', 'jetpack' ); ?></a>
 					</div>
 					<?php endif; ?>
 				</div>
--- views/admin/connection-modal-template.php
+++ views/admin/connection-modal-template.php
@@ -4,26 +4,39 @@
 	<div class="content-container <# if ( data.available) { #>modal-footer<# } #>">
 		<div id="my-connection-content" class="content">
 			<h2>Your Jetpack Connection</h2>
-			<# if ( data.isMasterUser && data.isAdmin ) { #>
-				<p>You are the master user. <br>
-					<strong>wpcom account info: </strong><br>
-					Username: {{{ data.masterComData.login }}}<br>
-					wpcom email: {{{ data.masterComData.email }}}</p>
-				<a href="<?php echo wp_nonce_url( Jetpack::admin_url( 'action=disconnect' ), 'jetpack-disconnect' ); ?>" onclick="return confirm('<?php echo htmlspecialchars( __('Are you sure you want to disconnect from WordPress.com?', 'jetpack'), ENT_QUOTES ); ?>');"><?php esc_html_e( 'Disconnect from WordPress.com', 'jetpack' ); ?></a>
-			<# } else { #>
-				<p>Primary connection details:<br>
-					<small>Site Administrator: {{{ data.masterUserLink }}}<br> WordPress.com: {{{ data.masterComData.login }}} / {{{ data.masterComData.email }}}</small></p>
-				<# if ( data.isUserConnected && data.isAdmin && data.isActive ) { #>
-					<p>You connection details:<br>
-					<small>WordPress.com: {{{ data.userComData.login }}} / {{{ data.userComData.email }}}</small></p>
-
-					<a id="set-self-as-master" class="button primary"><?php esc_html_e( 'Make primary account', 'jetpack' ); ?></a><span class="spinner"></span>
-					<a href="<?php echo wp_nonce_url( Jetpack::admin_url( 'action=unlink' ), 'jetpack-unlink' ); ?>"><?php esc_html_e( ' Disconnect your account', 'jetpack' ); ?></a>
+			<table>
+				<tr>
+					<th>User</th>
+					<th>WordPress.com Account</th>
+					<th>Actions</th>
+				</tr>
+				<# if ( data.isMasterUser && data.isAdmin ) { #>
+				<tr>
+					<td><strong><?php esc_html_e('You (primary)', 'jetpack'); ?></strong></td>
+					<td title="user: {{{ data.masterComData.login }}} / email: {{{ data.masterComData.email }}} "><strong>{{{ data.masterComData.login }}}</strong></td>
+					<td><a class="button" href="<?php echo wp_nonce_url( Jetpack::admin_url( 'action=disconnect' ), 'jetpack-disconnect' ); ?>" onclick="return confirm('<?php echo htmlspecialchars( __('Are you sure you want to disconnect from WordPress.com?', 'jetpack'), ENT_QUOTES ); ?>');"><?php esc_html_e( 'Disconnect', 'jetpack' ); ?></a></td>
+				</tr>
 				<# } else { #>
-					<p>Your user account is not linked to WordPress.com</p>
-					<a href="<?php echo Jetpack::init()->build_connect_url() ?>" class="download-jetpack"><?php esc_html_e( 'Link your account to WordPress.com', 'jetpack' ); ?></a>
+				<tr>
+					<td><strong>{{{ data.masterUserLink }}}<?php esc_html_e(' (primary)', 'jetpack'); ?></strong></td>
+					<td title="user: {{{ data.masterComData.login }}} / email: {{{ data.masterComData.email }}} "><strong>{{{ data.masterComData.login }}}</strong></td>
+					<td>&nbsp;</td>
+				</tr>
+					<# if ( data.isActive && data.isAdmin ) { #>
+						<tr>
+							<td><?php esc_html_e('You', 'jetpack'); ?></td>
+							<# if ( data.isUserConnected ) { #>
+								<td title="user: {{{ data.userComData.login }}} / email: {{{ data.userComData.email }}} "><strong>{{{ data.userComData.login }}}</strong></td>
+								<td><a id="set-self-as-master" class="button primary"><?php esc_html_e( 'Make primary account', 'jetpack' ); ?></a><span class="spinner"></span><a class="button" href="<?php echo wp_nonce_url( Jetpack::admin_url( 'action=unlink' ), 'jetpack-unlink' ); ?>"><?php esc_html_e( ' Disconnect', 'jetpack' ); ?></a></td>
+							<# } else { #>
+								<td>&nbsp;</td>
+								<td><a href="<?php echo Jetpack::init()->build_connect_url() ?>" class="button"><?php esc_html_e( 'Link your account', 'jetpack' ); ?></a></td>
+							<# } #>
+
+						</tr>
+					<# } #>
 				<# } #>
-			<# } #>
+			</table>
 		</div>
 	</div>
 </script>

d03f53d6cd3ccdc20201cd043a792af49ecec8fe
Derek Smart <smart@automattic.com>
2015-06-01 13:39:26 -0600

    ID Crisis: don't allow disconnect or syncing while on staging site
    New Function that simply returns bool whether or not the site has been set as a staging site.
    
    We don't want to sync any content to the live site from the staging site
    and disconnecting from a staging site would disconnect the live site, which is not ideal.

--- class.jetpack-sync.php
+++ class.jetpack-sync.php
@@ -168,6 +168,11 @@ class Jetpack_Sync {
 			return false;
 		}
 
+		// Don't sync anything from a staging site.
+		if ( Jetpack::jetpack_is_staging_site() ) {
+			return false;
+		}
+
 		$sync_data = $this->get_common_sync_data();
 
 		$wp_importing = defined( 'WP_IMPORTING' ) && WP_IMPORTING;

a3dfdddbfec4982eefabe15d8184017239700ba8
jeffgolenski <jeffgolenski@gmail.com>
2015-06-05 09:52:16 -0400

    Style adjustments for small mobile
    Style adjustments for small mobile

--- views/admin/my-connection-page.php
+++ views/admin/my-connection-page.php
@@ -23,7 +23,7 @@
 						<# console.log( data.potentialPrimaries ); #>
 
 							<?php // left col ?>
-							<div class="j-col j-lrg-4 j-md-6 j-sm-6 jp-user">
+							<div class="j-col j-lrg-4 j-md-6 j-sm-12 jp-user">
 								<h3 title="<?php _e( 'Username', 'jetpack' ); ?>"><?php _e( 'Site Username', 'jetpack' ); ?></h3>
 								<div class="user-01">
 									{{{ data.userGrav }}} {{{ data.connectionLogic.adminUsername }}}
@@ -31,7 +31,7 @@
 							</div>
 
 							<?php // middle col ?>
-							<div class="j-col j-lrg-4 j-md-6 j-sm-6 wp-user">
+							<div class="j-col j-lrg-4 j-md-6 j-sm-12 wp-user">
 								<h3 title="<?php _e( 'WordPress.com Username', 'jetpack' ); ?>"><?php _e( 'WordPress.com Username', 'jetpack' ); ?></h3>
 								<div class="wpuser-02">
 									<# if ( data.connectionLogic.isUserConnected ) { #>
@@ -69,7 +69,7 @@
 							<div class="j-row">
 
 								<?php //left col ?>
-								<div class="j-col j-lrg-4 j-md-6 j-sm-6 jp-user">
+								<div class="j-col j-lrg-4 j-md-6 j-sm-12 jp-user">
 									<h3 title="<?php _e( 'Primary User', 'jetpack' ); ?>"><?php _e( 'Primary User', 'jetpack' ); ?></h3>
 									<div class="user-01">
 										{{{ data.masterUserGrav }}} {{{ data.connectionLogic.masterUserLink }}}
@@ -77,7 +77,7 @@
 								</div>
 
 								<?php // middle col ?>
-								<div class="j-col j-lrg-4 j-md-6 j-sm-6 wp-user">
+								<div class="j-col j-lrg-4 j-md-6 j-sm-12 wp-user">
 									<h3 title="<?php _e( 'WordPress.com Username', 'jetpack' ); ?>"><?php _e( 'WordPress.com Username', 'jetpack' ); ?></h3>
 									<div class="wpuser-02">
 										<span>{{{ data.masterComData.login }}}</span>
